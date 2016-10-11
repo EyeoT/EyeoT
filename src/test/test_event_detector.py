5aeca599616ae389365c60cd57ce5b25187bbc89
genagain <ohta.g@husky.neu.edu>
2016-10-10 22:07:54 -0400

    correctly importing eventdetector into test

--- src/test/test_event_detector.py
+++ src/test/test_event_detector.py
@@ -1,1 +1,11 @@
-import event_detector
+from eventdetector import eventdetector
+import pytest
+import mock
+
+class TestEventDetector:
+
+    def test_initialize(self):
+        #event_detector = eventdetector.EventDetector()
+        import pdb;pdb.set_trace()
+        print event_detector
+
