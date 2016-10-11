edde7daff3d6764ba8a9bb74505293b3cf7360c5
Jenna Czeck <jennaczeck@gmail.com>
2016-10-11 10:22:30 -0400

    Adding comments

--- src/event_detector.py
+++ src/event_detector.py
@@ -22,27 +22,35 @@ class EventDetector:
         self.sub.setsockopt(zmq.SUBSCRIBE, '')
 
     def detect_blink(self, seconds_to_wait):
+        """ Detects when a blink happens for the specified number of seconds
+        """
         self.sub.setsockopt(zmq.SUBSCRIBE, 'pupil.')
         stay = True
+        # While we are still looking for the blink
         while stay:
             topic, msg = self.sub.recv_multipart()
             msg = loads(msg)
-            if msg['confidence'] == 0:
-                conf_queue = [0, 0, 0, 0, 0]
-                print('Blink')
-                start_blink = time.time()
+            # When the confidence drops below .5, we assume the
+            # eyes are closed and a blink has begun
+            if msg['confidence'] < .5:
+                conf_queue = [0, 0, 0, 0, 0]  # Reset our confidence queue
+                start_blink = time.time()  # Time when the blink began
+                # While the blink has not lasted for specified time
                 while ((time.time() - start_blink) < seconds_to_wait):
                     topic, msg = self.sub.recv_multipart()
                     msg = loads(msg)
                     confidence = msg['confidence']
                     stay = False
-                    print(confidence)
-                    conf_queue.pop(0)
+                    conf_queue.pop(0)  # Take out first item
+                    # If confidence is high, add a 1 to queue
                     if confidence > .2:
                         conf_queue.append(1)
-                        if sum(conf_queue) > 3:
+                        # When sum of the queue exceeds 2 we determine eyes
+                        # have been opened and blink has ended
+                        if sum(conf_queue) > 2:
                             stay = True
                             break
+                    # If confidence is low, add a 0 to the queue
                     else:
                         conf_queue.append(0)
 
