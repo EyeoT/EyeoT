a15ec7e834e4e93bb668fccc6efd61f5c6cd86f7
genagain <ohta.g@husky.neu.edu>
2016-10-15 13:11:06 -0400

    tested if two EventDetector attributes are certain zmq classes

--- src/test/test_event_detector.py
+++ src/test/test_event_detector.py
@@ -1,12 +1,14 @@
 from eventdetector import eventdetector
 import unittest
-import mock
+import zmq
 
 class EventDetectorTestCase(unittest.TestCase):
 
   def test_instantiation(self):
     import ipdb; ipdb.set_trace()
     event_detector = eventdetector.EventDetector()
+    assert isinstance(event_detector.context, zmq.sugar.context.Context)
+    assert isinstance(event_detector.req, zmq.sugar.socket.Socket)
 
 
 if __name__ == '__main__':
