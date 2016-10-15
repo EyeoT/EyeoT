1cec94ca6d849f4f09c360831765ab336a337bdf
genagain <ohta.g@husky.neu.edu>
2016-10-15 13:24:08 -0400

    getting rid of debugger

--- src/test/test_event_detector.py
+++ src/test/test_event_detector.py
@@ -5,7 +5,6 @@ import zmq
 class EventDetectorTestCase(unittest.TestCase):
 
   def test_instantiation(self):
-    import ipdb; ipdb.set_trace()
     event_detector = eventdetector.EventDetector()
     assert isinstance(event_detector.context, zmq.sugar.context.Context)
     assert isinstance(event_detector.req, zmq.sugar.socket.Socket)
