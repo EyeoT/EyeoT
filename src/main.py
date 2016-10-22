a075f55918dcc39d66e518413ece089222df0225
Jenna Czeck <jennaczeck@gmail.com>
2016-10-22 13:20:23 -0400

    Adding beginning of threading for main and event_detection into main, also allowing for longer lines

--- src/main.py
+++ src/main.py
@@ -1,13 +1,41 @@
-def initialize_pupil():
-    #TODO: start pupil
+import multiprocessing
 
-def idle():
-    #TODO: Monitor for long blink
+from event_detection import EventDetector
+
+
+def initialize():
+    """ Initialize creates all necessary objects for main
+    """
+    # TODO: start audio - thread?
+    # TODO: start ble - get list of nearby shit
+    event_detector = EventDetector()
+    return event_detector
+
+
+def idle(event_detector):
+    blink_proc = multiprocessing.Process(target=event_detector.detect_blink, args=(3,))
+    blink_proc.start()
+    blink_proc.join()
+    print('Blink Detected')
+    # TODO: Bluetooth callback
 
 
 def wake():
-    #TODO: Device control
+    # TODO: Device control
+    # TODO: DAQ_fixate (n seconds, mu, sigma) Machine Learning
+    print('Wake mode')
+
+
+def all_systems_good():
+    # TODO: Make sure things are working
+    return True
+
 
 if __name__ == "__main__":
-    initialize_pupil()
-    #TODO: setup main of main.py
+    event_detector = initialize()
+
+    while True:
+        idle(event_detector)
+        print('Idle finished')
+        wake()
+        print('Wake finished')
