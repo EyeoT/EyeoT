c957c3aad88218aa507a552a1c74d96609afa6b7
Jenna Czeck <jennaczeck@gmail.com>
2016-11-19 15:18:43 -0500

    Added more logic to main and more stub functions to event_stub

--- src/event_stub.py
+++ src/event_stub.py
@@ -13,3 +13,13 @@ class EventDetector:
     def detect_blink(self, seconds_to_wait=3):
         sleep(seconds_to_wait)
         print('Blinked')
+
+    def grab_frames(self, num_frames=1):
+        print('Frame grabbed')
+
+    def detect_gaze(self):
+        print('Gaze detected')
+
+    def detect_controls(self):
+        print('Controls detection')
+        return 0
